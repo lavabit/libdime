d4d20633929e262757270ed439b8ba6610b4820f
LBiv <ivan@lavabitllc.com>
2015-07-18 13:01:25 -0500

    Added unit tests for parsing the envelope chunks (origin, destination)

--- check/dmessage/checks.c
+++ check/dmessage/checks.c
@@ -4,6 +4,7 @@
 int main(void) {
 
 	SRunner *sr = srunner_create(suite_check_dmsg());
+	srunner_add_suite(sr, suite_check_parser());
 	srunner_run_all(sr, CK_ENV);
 	int nr_failed = srunner_ntests_failed(sr);
 	srunner_free(sr);
--- check/dmessage/checks.h
+++ check/dmessage/checks.h
@@ -1,3 +1,4 @@
 #include "../check-dime.h"
 
 Suite *suite_check_dmsg(void);
+Suite *suite_check_parser(void);
