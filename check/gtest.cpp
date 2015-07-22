e5318a0bf98d24ed0963e593c5d2d028efda43af
Zachary Michaels <mikezackles@gmail.com>
2015-07-21 22:16:01 -0400

    TAP output for googletest

--- check/gtest.cpp
+++ check/gtest.cpp
@@ -1,6 +1,13 @@
 #include "gtest/gtest.h"
+#include "tap.h"
 
-int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+GTEST_API_ int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+
+  // Delete the default listener
+  delete listeners.Release(listeners.default_result_printer());
+  listeners.Append(new tap::TapListener());
+  return RUN_ALL_TESTS();
 }
