4c758d36587ae1c36bb44f09dbfe20f97f2e9a8e
Zachary Michaels <mikezackles@gmail.com>
2015-07-22 20:24:15 -0400

    Swap TAP for XML, custom gtest driver
    The TAP driver doesn't seem terribly reliable, and googletest's default
    output is much better.  JUnit-compatible XML data is logged in case it's
    needed by other tools.  This seems optimal to me unless there's a tool
    we need that needs TAP and doesn't accept JUnit XML.
    
    I've modified the default Automake test driver so that it will preserve
    googletest's colorized (when appropriate) output.

--- check/gtest.cpp
+++ check/gtest.cpp
@@ -4,10 +4,5 @@
 GTEST_API_ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
 
-  ::testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
-
-  // Delete the default listener
-  delete listeners.Release(listeners.default_result_printer());
-  listeners.Append(new tap::TapListener());
   return RUN_ALL_TESTS();
 }
