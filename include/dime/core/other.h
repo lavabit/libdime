d8cf915c30ad172fd64e7cc85fb4f36d0d65ce48
Zachary Michaels <mikezackles@gmail.com>
2015-08-05 13:31:39 -0400

    Remove core entirely

--- include/dime/core/other.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAGMA_CORE_OTHER_H
-#define MAGMA_CORE_OTHER_H
-
-uint64_t _rand_get_uint64(void);
-
-#endif
--- libs/core/global.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- * @brief   Functions for handling the global configuration.
- */
-
-#include <dime/core/magma.h>
-
-__thread char threadBuffer[1024];
-magma_t magma = { .config.file = "magma.config" };
